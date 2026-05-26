66fbb077e2f11224d4ff24a03e3621642489f50c
Ondrej Profant <ondrej.profant@gmail.com>
2026-05-26 22:12:08 +0200

    chore: dependencies

--- scripts/update_assets.sh
+++ scripts/update_assets.sh
@@ -10,12 +10,19 @@ set -e
 STATIC_DIR="internal/web/static"
 mkdir -p "$STATIC_DIR"
 
-HTMX_VERSION="latest" # 2.0.8
+HTMX_VERSION="latest" # 2.0.10
+TABULATOR_VERSION="@6.4.0" #empty means latest
 
 echo "[GET] Downloading water.css..."
 curl -sL "https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" -o "$STATIC_DIR/water.css"
 
 echo "[GET] Downloading htmx.min.js..."
 curl -sL "https://unpkg.com/htmx.org@$HTMX_VERSION/dist/htmx.min.js" -o "$STATIC_DIR/htmx.min.js"
+
+echo "[GET] Downloading tabulator.min.css..."
+curl -sL "https://unpkg.com/tabulator-tables$TABULATOR_VERSION/dist/css/tabulator.min.css" -o "$STATIC_DIR/tabulator.min.css"
+
+echo "[GET] Downloading tabulator.min.js..."
+curl -sL "https://unpkg.com/tabulator-tables$TABULATOR_VERSION/dist/js/tabulator.min.js" -o "$STATIC_DIR/tabulator.min.js"
 
 echo "[OK] All static assets updated successfully!"
